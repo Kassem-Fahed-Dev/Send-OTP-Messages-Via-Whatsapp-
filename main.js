0ae42d6a5c5e33bbb2c14648e1632b8bddac7c42
Kassem-Fahed-Dev <kassemfahed2003@gmail.com>
2025-05-26 17:35:15 +0300

    update main.js

--- main.js
+++ main.js
@@ -24,7 +24,8 @@ session.on("ready", () => {
 	console.log("Login successful");
 });
 app.get("/", (req, res) => {
-	res.send("Hello World");
+	//res.send("Hello World");
+	res.render('qr',{title:'test rendering'})
 });
 app.get("/whatsapp/login", async (req, res) => {
 	if (tokenQr === null) return res.send("please try later");
